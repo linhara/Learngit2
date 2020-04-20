22285c3555c539cf9ee94dcc1e305c1e279f31a5
Linus <lins_haraldsson@hotmail.com>
2020-04-20 20:30:28 +0200

    ok nu funkar de faktsikt, kanske

--- Neural_Network/main.py
+++ Neural_Network/main.py
@@ -1,13 +1,12 @@
 import numpy as np
 import Layers
 inp = np.array([1, 2, 3, 3, 4]).T
-structList = [len(inp), 5, 3, 2]
-expected = [0.5, 1]
-rows = 4
-col = 5
+structList = [len(inp), 5, 3]
+expected = [0, 1, 0]
 layers = []
 activation_of_each_layer = [inp]
 learn_rate = 0.1
+#inputs = [[0, 0, 1],[0, 1, 1],[1, 0, 0],[1, 1, 0],[1, 0, 1],[1, 1, 1]]
 
 def main():
     init_struct()
@@ -23,8 +22,8 @@ def main():
 
     # ----------------------------------------------
 
-    #print(activation_of_each_layer)
-    #print(cost)
+    print(activation_of_each_layer)
+    print(cost)
 
 
 def init_struct():
@@ -36,7 +35,7 @@ def run_network(inp):
     activations = inp
     for layer in layers:
         activations = layer.step_forward(activations)
-        activation_of_each_layer.append(activations)
+        activation_of_each_layer.append(activations)        #do i ever use this shit???
 
 
 def back_prop(i):
@@ -44,12 +43,10 @@ def back_prop(i):
     hopefully_gradient = layers[i].received_activations[:1] * layers[i].error
     layers[i].weights += -learn_rate * hopefully_gradient[:, np.newaxis]
 
-
 def cor_output_layer(cost):                                         #denna funktionen borde inte behövas
     layers[-1].error = cost
-    hopefully_gradient = layers[-2].activations[1:] * cost
+    hopefully_gradient = layers[-1].received_activations[:1] * cost
     layers[-1].weights += -learn_rate * hopefully_gradient[:, np.newaxis]
-
 
 def calc_cost_prime(ans, expected):
     return 2*(ans - expected)
